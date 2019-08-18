327d105c9590267b2d43c6b8a3554054d2b0b969
marcWong <yaowang95@pku.edu.cn>
2019-08-18 21:36:48 +0800

    update gitignore

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 conda create -n PointMVSNet python=3.6
 source activate PointMVSNet
-conda install pytorch torchvision cudatoolkit=9.0 -c pytorch
+conda install pytorch torchvision cudatoolkit=9.0 -c pytorch==1.0.1
 conda install -c anaconda pillow
-pip install -r requirements
+pip install -r requirements.txt
